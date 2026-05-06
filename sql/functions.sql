ed8a0773817a0413e49a06934a044fb3fa8a182e
Tamana <tamana7522@gmail.com>
2026-05-06 22:26:27 +0530

    Modified calculate_fine function
    Added a function to calculate fines based on issue and return dates.

--- sql/functions.sql
+++ sql/functions.sql
@@ -1,9 +1,18 @@
-SELECT i.issue_id, m.name, b.title,
-       i.issue_date, i.return_date, i.status
-FROM ISSUE i
-JOIN MEMBERS m ON i.member_id = m.member_id
-JOIN BOOKS b ON i.book_id = b.book_id
-WHERE 
-    (i.status = 'ISSUED' AND SYSDATE - i.issue_date > 7)
-    OR
-    (i.status = 'RETURNED' AND i.return_date - i.issue_date > 7);
+CREATE OR REPLACE FUNCTION calculate_fine(
+p_issue_date DATE,
+p_return_date DATE -- Pass SYSDATE for still-issued books
+)
+RETURN NUMBER
+IS
+v_days_held NUMBER;
+v_grace CONSTANT NUMBER := 7; -- free days
+v_rate CONSTANT NUMBER := 1; -- Rs. per overdue day
+v_fine NUMBER := 0;
+BEGIN
+v_days_held := TRUNC(NVL(p_return_date, SYSDATE)) - TRUNC(p_issue_date);
+IF v_days_held > v_grace THEN
+v_fine := (v_days_held - v_grace) * v_rate;
+END IF;
+RETURN v_fine;
+END calculate_fine;
+/
